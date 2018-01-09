8f5069478f2735be44e934450a39676fa66f7b70
accentureapis <31919314+accentureapis@users.noreply.github.com>
2018-01-09 13:00:16 +0530

    Update Evaluation.scala

--- src/main/scala/Evaluation.scala
+++ src/main/scala/Evaluation.scala
@@ -7,7 +7,7 @@ import io.prediction.controller.EngineParamsGenerator
 import io.prediction.controller.Evaluation
 
 case class Accuracy
-  extends AverageMetric[EmptyEvaluationInfo, Query, PredictedResult, ActualResult] {
+  extends AverageMetric(EmptyEvaluationInfo, Query, PredictedResult, ActualResult) {
   def calculate(query: Query, predicted: PredictedResult, actual: ActualResult)
   : Double = (if (predicted.label == actual.label) 1.0 else 0.0)
 }
